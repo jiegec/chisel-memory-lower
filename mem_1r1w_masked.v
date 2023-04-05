b84df626aa74fcf7199afc8c10484a19d2560d74
Jiajie Chen <c@jia.je>
2023-04-05 08:44:27 +0800

    Add 1r1w masked

---
+++ mem_1r1w_masked.v
@@ -0,0 +1,34 @@
+// Generate by chisel-memory-lower
+// Config(name='mem_1r1w_masked', depth='32', width='64', ports='mwrite,read', mask_gran='8')
+module mem_1r1w_masked (
+  input [4:0] R0_addr,
+  input R0_en,
+  input R0_clk,
+  output [63:0] R0_data,
+  input [4:0] W0_addr,
+  input W0_en,
+  input W0_clk,
+  input [63:0] W0_data,
+  input [7:0] W0_mask
+);
+  xpm_memory_sdpram #(
+    .ADDR_WIDTH_A(5),
+    .ADDR_WIDTH_B(5),
+    .BYTE_WRITE_WIDTH_A(8),
+    .MEMORY_SIZE(2048),
+    .READ_DATA_WIDTH_B(64),
+    .READ_LATENCY_B(1),
+    .WRITE_DATA_WIDTH_A(64)
+  ) xpm_memory_sdpram_inst (
+    .dina(W0_data),
+    .addra(W0_addr),
+    .ena(W0_en),
+    .wea(W0_mask),
+    .clka(W0_clk),
+    .addrb(R0_addr),
+    .clkb(R0_clk),
+    .enb(R0_en),
+    .doutb(R0_data),
+    .rstb(1'b0),
+  );
+endmodule
---
+++ mem_1rw.v
@@ -0,0 +1,27 @@
+// Generate by chisel-memory-lower
+// Config(name='mem_1rw', depth='32', width='64', ports='rw', mask_gran=None)
+module mem_1rw (
+  input [4:0] RW0_addr,
+  input RW0_en,
+  input RW0_clk,
+  input RW0_wmode,
+  input [63:0] RW0_wdata,
+  output [63:0] RW0_rdata
+);
+  xpm_memory_spram #(
+    .ADDR_WIDTH_A(5),
+    .BYTE_WRITE_WIDTH_A(64),
+    .MEMORY_SIZE(2048),
+    .READ_DATA_WIDTH_A(64),
+    .READ_LATENCY_A(1),
+    .WRITE_DATA_WIDTH_A(64)
+  ) xpm_memory_spram_inst (
+    .douta(RW0_rdata),
+    .addra(RW0_addr),
+    .clka(RW0_clk),
+    .dina(RW0_wdata),
+    .ena(RW0_en),
+    .rsta(1'b0),
+    .wea(RW0_wmode),
+  );
+endmodule
