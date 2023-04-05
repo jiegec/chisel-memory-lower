26e560b16977b0c3919c73d7b58164b26b3e06b4
Jiajie Chen <c@jia.je>
2023-04-05 10:15:05 +0800

    Update comments url

--- chisel_memory_lower/arm.py
+++ chisel_memory_lower/arm.py
@@ -1,9 +1,12 @@
 from chisel_memory_lower.utils import generate_header
 from chisel_memory_lower.parser import Config
 from collections import namedtuple
+import yaml
 
 
 def generate(config: Config, arm_config: str):
+    cfg = yaml.load(open(arm_config, 'r'), Loader=yaml.Loader)
+    ip = cfg['ip']
     with open(f'{config.name}_arm.v', 'w') as f:
         ports = set(config.ports.split(','))
         depth = int(config.depth)
@@ -12,8 +15,17 @@ def generate(config: Config, arm_config: str):
         header = generate_header(config, 'arm')
         print(header, file=f)
 
+        types = ''
         if ports == {"read", "write"}:
             # 1R1W
-            pass
+            types = ['1r1w', '1r1w_masked']
+        elif ports == {"rw"}:
+            # 1RW
+            types = ['1rw']
+        elif ports == {"read", "mwrite"}:
+            # 1R1W Masked
+            types = ['1r1w_masked']
+        candidates = list(filter(lambda c: c['type'] in types, ip))
+        print(candidates)
         print(f'endmodule', file=f)
         pass
