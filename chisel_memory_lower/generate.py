93bcab710b77a20f4f4ec2abf3a4dc556176c79e
Jiajie Chen <c@jia.je>
2023-04-08 15:46:15 +0800

    Add vivado path arg

--- chisel_memory_lower/generate.py
+++ chisel_memory_lower/generate.py
@@ -9,12 +9,13 @@ import chisel_memory_lower.arm
 @click.argument('config', type=click.Path(exists=True))
 @click.option('-a', '--arm-config', 'arm_config', help="Configuration file for IP")
 @click.option('-t', '--tb', is_flag=True, help="Generate testbench")
-def generate(target: str, config: str, arm_config: str, tb: bool):
+@click.option('-v', '--vivado-path', 'vivado_path', help="Path to vivado installation", default="/opt/Xilinx/Vivado/2020.2")
+def generate(target: str, config: str, arm_config: str, tb: bool, vivado_path: str):
     content = open(config, 'r').read()
     for mem in parse(content):
         print(f'Generating {mem}')
         if target == "xilinx":
-            chisel_memory_lower.xilinx.generate(mem, tb)
+            chisel_memory_lower.xilinx.generate(mem, tb, vivado_path)
         elif target == "arm":
             chisel_memory_lower.arm.generate(mem, arm_config, tb)
         else:
